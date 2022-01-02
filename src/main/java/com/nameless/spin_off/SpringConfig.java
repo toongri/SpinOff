02345cf061c3415f96fa6d5824bf1511ff6e3552
Toong_laptop <jhkim03284@gmail.com>
2022-01-02 23:05:24 +0900

    10th commit : querydsl 설정변경

--- src/main/java/com/nameless/spin_off/SpringConfig.java
+++ src/main/java/com/nameless/spin_off/SpringConfig.java
@@ -1,7 +1,6 @@
 package com.nameless.spin_off;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
