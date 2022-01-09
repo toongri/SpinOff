c9e0cdf59c3474b258e6f61062cddf0e7f5693f5
Toong_laptop <jhkim03284@gmail.com>
2022-01-09 23:49:13 +0900

    14th commit : sql formatter 구현

---
+++ src/main/java/com/nameless/spin_off/config/P6spy/P6spySqlFormatter.java
@@ -0,0 +1,40 @@
+package com.nameless.spin_off.config.P6spy;
+
+import com.p6spy.engine.logging.Category;
+import com.p6spy.engine.spy.appender.MessageFormattingStrategy;
+import org.hibernate.engine.jdbc.internal.FormatStyle;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class P6spySqlFormatter implements MessageFormattingStrategy {
+    @Override
+    public String formatMessage(int connectionId, String now, long elapsed, String category, String prepared, String sql, String url) {
+
+        sql = formatSql(category, sql);
+        Date currentDate = new Date();
+        SimpleDateFormat format1 = new SimpleDateFormat("yy.MM.dd HH:mm:ss");
+
+        //return now + "|" + elapsed + "ms|" + category + "|connection " + connectionId + "|" + P6Util.singleLine(prepared) + sql;
+        return format1.format(currentDate) + " | "+ "OperationTime : "+ elapsed + "ms" + sql;
+    }
+
+    private String formatSql(String category,String sql) {
+
+        if(sql ==null || sql.trim().equals("")) return sql;
+
+        // Only format Statement, distinguish DDL And DML
+        if (Category.STATEMENT.getName().equals(category)) {
+            String tmpsql = sql.trim().toLowerCase(Locale.ROOT);
+            if(tmpsql.startsWith("create") || tmpsql.startsWith("alter") || tmpsql.startsWith("comment")) {
+                sql = FormatStyle.DDL.getFormatter().format(sql);
+            }else {
+                sql = FormatStyle.BASIC.getFormatter().format(sql);
+            }
+            sql = "|\nToongriFormatSql(P6Spy sql,Hibernate format):"+ sql;
+        }
+
+        return sql;
+    }
+}
