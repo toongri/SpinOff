1cf5afbd3a0446f206009d1c3e1fcaa429f25de7
Toong_laptop <jhkim03284@gmail.com>
2022-03-16 22:15:58 +0900

    95th commit : swagger paging 적용

--- src/main/java/com/nameless/spin_off/config/SwaggerConfig.java
+++ src/main/java/com/nameless/spin_off/config/SwaggerConfig.java
@@ -1,20 +1,33 @@
 package com.nameless.spin_off.config;
 
+import com.fasterxml.classmate.TypeResolver;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.domain.Pageable;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.AlternateTypeRules;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.List;
+
 @Profile("real")
 @Configuration
+@RequiredArgsConstructor
 @EnableSwagger2
 public class SwaggerConfig {
+
+    private final TypeResolver typeResolver;
 
     private String version;
     private String title;
@@ -28,6 +41,9 @@ public class SwaggerConfig {
         description = "테스트용";
 
         return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .alternateTypeRules(AlternateTypeRules
+                        .newRule(typeResolver.resolve(Pageable.class), typeResolver.resolve(Page.class)))
                 .select()
                 .apis(RequestHandlerSelectors.any()) // 현재 RequestMapping으로 할당된 모든 URL 리스트를 추출
 //                .apis(RequestHandlerSelectors.basePackage("com.nameless.spin_off.controller/api"))
@@ -42,5 +58,19 @@ public class SwaggerConfig {
                 .version(version)
                 .description(description)
                 .build();
+    }
+
+    @Getter
+    @Setter
+    @ApiModel
+    static class Page {
+        @ApiModelProperty(value = "페이지 번호(0..N)")
+        private Integer page;
+
+        @ApiModelProperty(value = "페이지 크기", allowableValues="range[0, 100]")
+        private Integer size;
+
+        @ApiModelProperty(value = "정렬(사용법: 컬럼명,ASC|DESC ex: sort=id,asc)")
+        private List<String> sort;
     }
 }
