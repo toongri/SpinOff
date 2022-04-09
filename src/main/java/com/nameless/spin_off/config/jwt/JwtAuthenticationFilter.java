d9f7680f31796246799835a86bf894f9bd67b6c0
Toong_laptop <jhkim03284@gmail.com>
2022-04-09 14:10:00 +0900

    125th commit : 스웨거 설정 변경

--- src/main/java/com/nameless/spin_off/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/nameless/spin_off/config/jwt/JwtAuthenticationFilter.java
@@ -24,14 +24,16 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = jwtTokenProvider.resolveToken(request);
         // 유효한 토큰인지 확인합니다.
-        if (!token.equals("")) {
-            if (jwtTokenProvider.validateTokenExpiration(token)) {
-                // 토큰이 유효하면 토큰으로부터 유저 정보를 받아옵니다.
-                Authentication auth = jwtTokenProvider.getAuthentication(token);
-                // SecurityContext에 Authentication 객체를 저장합니다.
-                SecurityContextHolder.getContext().setAuthentication(auth);
-            } else {
-                throw new AuthenticationEntryException(ErrorEnum.AUTHENTICATION_ENTRY);
+        if (token != null) {
+            if (!token.equals("")) {
+                if (jwtTokenProvider.validateTokenExpiration(token)) {
+                    // 토큰이 유효하면 토큰으로부터 유저 정보를 받아옵니다.
+                    Authentication auth = jwtTokenProvider.getAuthentication(token);
+                    // SecurityContext에 Authentication 객체를 저장합니다.
+                    SecurityContextHolder.getContext().setAuthentication(auth);
+                } else {
+                    throw new AuthenticationEntryException(ErrorEnum.AUTHENTICATION_ENTRY);
+                }
             }
         }
         filterChain.doFilter(request, response);
